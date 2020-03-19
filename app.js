95a162f8bb8efb728b8b29f62479afdfb7aa06c0
Benjamin Brier <ben.brier14@gmail.com>
2020-03-19 13:25:35 -0400

    app will repeat what is said

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('.talk');
-const content = document.querySelector('.conteent');
+const content = document.querySelector('.content');
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
@@ -12,9 +12,20 @@ recognition.onresult = function(event) {
 	const current = event.resultIndex;
 	const transcript = event.results[current][0].transcript;
 	content.textContent = transcript;
+	readOutLoud(transcript);
 };
 
 //Add the Listener to button
 btn.addEventListener('click', () => {
 	recognition.start();
 });
+
+function readOutLoud(message) {
+	const speech = new SpeechSynthesisUtterance();
+	speech.text = message;
+	speech.volume = 1;
+	speech.rate = 1;
+	speech.pitch = 1;
+
+	window.speechSynthesis.speak(speech);
+}
