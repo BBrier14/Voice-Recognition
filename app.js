4037c4e08fabd85c429640d01a8eaf0614126f92
Benjamin Brier <ben.brier14@gmail.com>
2020-03-20 19:46:51 -0400

    added greetings, organized code

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const btn = document.querySelector('.talk');
 const content = document.querySelector('.content');
 
-//
-const greetings = [ 'Im good', 'Im doing good man', 'leave me alone' ];
+//----- Responses -----//
+const greetings = [
+	'I am good',
+	'I am doing good, man',
+	'leave me alone',
+	'All the better now that you asked.',
+	'Much better now that you are with me.',
+	'At minding my own business? Better than most people',
+	'I’ve been going through some crests and troughs in my life. Is everything stable at your end?',
+	'Cant complain',
+	'Living a dream'
+];
+
 const weather = [ 'weather is fine', 'you need a tan' ];
 
+//----- Opening and listening -----//
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
@@ -24,6 +36,7 @@ btn.addEventListener('click', () => {
 	recognition.start();
 });
 
+//----- Responding to Questions -----//
 function readOutLoud(message) {
 	const speech = new SpeechSynthesisUtterance();
 
